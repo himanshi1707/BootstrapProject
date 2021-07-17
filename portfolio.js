260c67ad6996b2804aeae8df4f3a05883f6c8cd7
himanshi1707 <72624727+himanshi1707@users.noreply.github.com>
2021-07-17 23:29:35 +0530

    Add bootstrap website files

--- portfolio.js
+++ portfolio.js
@@ -3,16 +3,15 @@ document.getElementById('loginbtn').addEventListener('click',function(){
     let email=document.getElementById('email').value;
     let message=document.getElementById('message').value;
 
-    debugger
+    
     document.getElementById('name_error').innerText='';
     document.getElementById('email_error').innerText='';
-    document.getElementById('message_error').innerText='';
-
+    
     if(username=='')
     {
         document.getElementById('name_error').innerText='Username field can not be empty';
         document.getElementById('name_error').style.color='red';
-        // console.log(document.getElementById('name_error').value)
+        
 
     }
     else if(email=='')
@@ -20,13 +19,9 @@ document.getElementById('loginbtn').addEventListener('click',function(){
         document.getElementById('email_error').innerText='Email field can not be empty';
         document.getElementById('email_error').style.color='red';
     }
-    else if(message=='')
-    {
-        document.getElementById('message_error').innerText='Message field can not be empty';
-        document.getElementById('message_error').style.color='red';
-    }
     else{
-        alert('Submitted');
+       var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+       myModal.show();
     }
 });
 
